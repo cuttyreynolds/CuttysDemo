0b69ef91deadb68ecfe78654a353a7f53f7e7309
cuttyreynolds <cutty.reynolds@pendo.io>
2024-07-24 13:48:50 -0700

    Update pendo.js

--- pendo.js
+++ pendo.js
@@ -1,4 +1,3 @@
-<script>
 (function(apiKey){
     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
     v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
@@ -35,4 +34,4 @@
             }
         });
 })('49a60ab6-2a04-4cd2-4127-1f6432ff4758');
-</script>
+
